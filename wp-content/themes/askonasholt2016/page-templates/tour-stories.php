83d5ddf6abb825ba2239099a7bf72344bb2dc15c
jamesburt85 <jamesburt85@gmail.com>
2016-11-23 15:56:10 +0000

    people/team page

--- wp-content/themes/askonasholt2016/page-templates/tour-stories.php
+++ wp-content/themes/askonasholt2016/page-templates/tour-stories.php
@@ -28,33 +28,7 @@ get_header(); ?>
             </header>
 
 
-            <div class="row">
 
-              <div class="small-12 columns">
-
-                <?php 
-
-                  echo "<ul class='artist-categories'>";
-                  // echo "<li></li>";
-                  // echo wp_list_categories( 'title_li'=>'Select category' );
-                  echo wp_list_categories( array(
-                          'taxonomy' => 'magazine-content-type',
-                          'orderby'  => 'name',
-                          'title_li' => '',
-                          'exclude'  => 1, // don't show uncategorised
-                          'show_option_all' => 'show all',
-                          'walker'       => new Walker_Category_Find_Parents(),
-                      ) );
-                    // $args = array(
-                    // 'title_li'=>'Select category',
-                    // )
-                  echo "</ul>";
-
-                ?>
-
-              </div>
-            
-            </div>
 
           </div>
 
@@ -64,7 +38,7 @@ get_header(); ?>
               $args = array(
 
                 'post_type'   => 'magazine',
-                'posts_per_page' => 4,
+                //'posts_per_page' => 4,
                 'tax_query' => array(
                     array(
                       'taxonomy' => 'magazine-content-type',
--- wp-content/themes/askonasholt2016/template-parts/content-people.php
+++ wp-content/themes/askonasholt2016/template-parts/content-people.php
@@ -11,33 +11,63 @@
 ?>
 
 <div id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry'); ?>>
-	<header>
-		<h2><a href="<?php //the_permalink(); ?>"><?php //the_title(); ?></a></h2>
-		<?php //foundationpress_entry_meta(); ?>
-	</header>
+
+	<?php //get_template_part( 'template-parts/center-text-hero' ); ?>
 
 	<?php
 
-	if ( have_rows('staff_member') ); { ?>
+	//if ( have_rows('staff_member') ); { ?>
 
-		<div>
+		<div class="row">
 
-			<?php while ( have_rows('staff_member') ) { the_row();
+			<?php //while ( have_rows('staff_member') ) { the_row();
 
-				$image = get_sub_field('staff_photo');
-				$position = get_sub_field('position');
+				//$image = get_sub_field('staff_photo');
+				$position = get_field('position');
+				$e_mail = get_field('e-mail');
+				$telephone_number = get_field('telephone_number');
+				$languages = get_field('languages');
 
-				?>
+			?>
 
-				<img src="<?php echo $image['url']; ?>">
-				<span><?php the_title(); ?></span>
-				<span><?php echo $position; ?></span>
+				<div class="entry-content small-12 medium-6 large-3 columns">
+
+					<div class=" magazine-item">
+						<?php if ( has_post_thumbnail() ) { ?>
+
+							<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
+							<div class="magazine-item-thumb" style="background-image: url('<?php echo $thumb['0'];?>')"></div>
+
+
+						<?php } 
+
+						else { ?>
+							
+							<div class="magazine-item-thumb" style="background-image: url('<?php bloginfo('template_directory'); ?>/assets/images/default.jpg');"></div>
+
+						<?php } ?>
+
+
+						<div class="magazine-item-container">
+							
+							<p class="magazine-item-header">
+								<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><br/>
+								<a href="mailto:<?php echo $e_mail; ?>?Subject=Hello%20again" target="_top"><?php echo $e_mail; ?></a><br/>
+								<span><?php echo $telephone_number; ?></span><br/>
+								<span><?php echo $languages; ?></span>
+							</p>
+								<?php the_excerpt( __( 'Continue reading...', 'foundationpress' ) ); ?>
+						</div>
+
+					</div>
+
+				</div>
 				
-			<?php } ?>
+			<?php //} ?>
 
 		</div>
 
-	<?php } ?>
+	<?php //} ?>
 
 	
 	<div class="entry-content">
