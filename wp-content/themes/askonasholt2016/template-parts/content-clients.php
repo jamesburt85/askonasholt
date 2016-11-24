de217b60004e2ca8c11b6004e39737341b7cb356
jamesburt85 <jamesburt85@gmail.com>
2016-11-24 12:20:55 +0000

    Adding Quick View to Clients Page

--- wp-content/themes/askonasholt2016/template-parts/content-clients.php
+++ wp-content/themes/askonasholt2016/template-parts/content-clients.php
@@ -21,14 +21,28 @@
 			<?php if ( has_post_thumbnail() ) { ?>
 
 				<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
-				<div class="magazine-item-thumb" style="background-image: url('<?php echo $thumb['0'];?>')"></div>
+				<div class="magazine-item-thumb" style="background-image: url('<?php echo $thumb['0'];?>')">
+					<div class="overlay">
+						<a href="#quicklook-<?php the_ID(); ?>" class="open-popup-link">
+							<i class="fa fa-eye" aria-hidden="true"></i>
+							<span>Quick Look</span>
+						</a>
+					</div>
+				</div>
 
 
 			<?php } 
 
 			else { ?>
 				
-				<div class="magazine-item-thumb" style="background-image: url('<?php bloginfo('template_directory'); ?>/assets/images/default.jpg');"></div>
+				<div class="magazine-item-thumb" style="background-image: url('<?php bloginfo('template_directory'); ?>/assets/images/default.jpg');">
+					<div class="overlay">
+						<a href="#quicklook-<?php the_ID(); ?>" class="open-popup-link">
+							<i class="fa fa-eye" aria-hidden="true"></i>
+							<span>Quick Look</span>
+						</a>
+					</div>
+				</div>
 
 			<?php } ?>
 
@@ -38,7 +52,7 @@
 					<?php echo wpdocs_custom_taxonomies_terms_links(); ?>
 					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><br/>
 				</p>
-					<?php the_excerpt( __( 'Continue reading...', 'foundationpress' ) ); ?>
+					<?php //the_excerpt( __( 'Continue reading...', 'foundationpress' ) ); ?>
 			</div>
 
 		</div>
@@ -46,10 +60,30 @@
 	</div>
 
 
+	<div id="quicklook-<?php the_ID(); ?>" class="quicklook-content mfp-hide">
+
+		<div class="quicklook-content-pic" style="background-image: url('<?php echo $thumb['0'];?>')"></div>
+		<div class="quicklook-content-profile">
+			<h3><?php the_title(); ?></h3>
+			<h6><?php echo wpdocs_custom_taxonomies_terms_links(); ?></h6>
+			<ul>
+				<li><a href="mailto:<?php echo $e_mail; ?>?Subject=Hello%20again" target="_top"><?php echo $e_mail; ?></a></li>
+
+			</ul>
+		</div>
+
+		<div class="quicklook-content-bio">
+			<?php the_content(); ?>
+		</div>
+		<div class="quicklook-content-links">
+			<a class="button" href="<?php the_permalink(); ?>">Go to Staff Page</a>	
+		</div>
+
+	</div>
 
 	
 	<div class="entry-content">
-		<?php the_content( __( 'Continue reading...', 'foundationpress' ) ); ?>
+		<?php //the_content( __( 'Continue reading...', 'foundationpress' ) ); ?>
 	</div>
 	<footer>
 		<?php $tag = get_the_tags(); if ( $tag ) { ?><p><?php the_tags(); ?></p><?php } ?>
