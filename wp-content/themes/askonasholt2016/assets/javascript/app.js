58f7bd0baab25e498d328259621bae57520f3633
adstyles <adstyles@gmail.com>
2016-12-01 10:42:38 +0000

    quick look fixed

--- wp-content/themes/askonasholt2016/assets/javascript/app.js
+++ wp-content/themes/askonasholt2016/assets/javascript/app.js
@@ -307,10 +307,16 @@ var map = null;
     });
 
 
-    $('.open-popup-link').magnificPopup({
-      type:'inline',
-      midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-    });
+    QuickLookInit = function(){
+
+      $('.open-popup-link').magnificPopup({
+        type:'inline',
+        midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+      });
+
+    }
+
+    QuickLookInit();
 
     //*********************************** 
     //***** SEARCH BAR TOGGLE UP ********
