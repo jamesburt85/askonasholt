527342dbe9bc1077d18f1972aca6b28c385694f2
jamesburt85 <jamesburt85@gmail.com>
2016-12-05 14:24:45 +0000

    Live On Demand Single

--- wp-content/themes/askonasholt2016/single-online.php
+++ wp-content/themes/askonasholt2016/single-online.php
@@ -63,7 +63,7 @@ get_header(); ?>
 			    </a>
 
 			</div>
-
+		<div class="artist-list">
 			<?php 
 
 				$artist = get_field('artist');
@@ -71,39 +71,41 @@ get_header(); ?>
 
 				if (!empty($artist)) {
 					foreach ($artist as $artist_id) { ?> 
+						
 
-						<div class="side-bar-artist"> <?php
-							# Get featured image id
-							$thumb_id = get_post_thumbnail_id($artist_id);
-							# If theere is not a featured image
-							if ( empty( $thumb_id)) {
-								$thumb_url = 'http://placehold.it/150x150';
-							# Yeay, we haven image ID
-							} else {
-								# Get the image from the image ID
-								$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail', true);
-								$thumb_url = $thumb_url_array[0];
-							}
-							//echo $thumb_url;
+							<div class="side-bar-artist"> <?php
+								# Get featured image id
+								$thumb_id = get_post_thumbnail_id($artist_id);
+								# If theere is not a featured image
+								if ( empty( $thumb_id)) {
+									$thumb_url = 'http://placehold.it/150x150';
+								# Yeay, we haven image ID
+								} else {
+									# Get the image from the image ID
+									$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail', true);
+									$thumb_url = $thumb_url_array[0];
+								}
+								//echo $thumb_url;
 
-							# Get post terms as array
-							$artist_types = get_the_terms( $artist_id, 'artist-type');
+								# Get post terms as array
+								$artist_types = get_the_terms( $artist_id, 'artist-type');
 
-							?>
-							
-							<img class="circle-thumb" src="<?php echo $thumb_url ?>">
-							
-							<div class="side-bar-artist-details">
-								<span class="side-bar-artist-name"><?php echo get_the_title( $artist_id) ?></span>&nbsp;<br/>
-							
-								<?php # If this artist has an artist-type
-								# - Will only EVER return the first result in the artist type array
-								//if ( !empty( $artist_types)): ?>
-									<span><?php //echo $artist_types[0]->name ?></span>
-								<?php //endif ?>
+								?>
+								
+								<img class="circle-thumb" src="<?php echo $thumb_url ?>">
+								
+								<div class="side-bar-artist-details">
+									<span class="side-bar-artist-name"><?php echo get_the_title( $artist_id) ?></span>&nbsp;<br/>
+								
+									<?php # If this artist has an artist-type
+									# - Will only EVER return the first result in the artist type array
+									//if ( !empty( $artist_types)): ?>
+										<span><?php //echo $artist_types[0]->name ?></span>
+									<?php //endif ?>
+								</div>
+
 							</div>
-
-						</div>
+						
 			<!-- 					<?php # If the artist has an artist type
 						if ( !empty( $artist_types)): ?>
 							<ul>
@@ -118,7 +120,7 @@ get_header(); ?>
 						<?php
 					}
 				} ?>
-
+			</div>
 			<h4 class="section-header">About the Performance</h4>
 
 			
