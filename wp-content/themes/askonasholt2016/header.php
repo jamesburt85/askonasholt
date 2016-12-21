ece52f5b760550975e2fa37d2cf15ced2c41a984
jamesburt85 <jamesburt85@gmail.com>
2016-12-21 12:01:37 +0000

    No more MegaMenu. Styling dropdowns

--- wp-content/themes/askonasholt2016/header.php
+++ wp-content/themes/askonasholt2016/header.php
@@ -95,11 +95,11 @@
 			</div>
 
 			<div class="top-bar-right">
-				<?php //foundationpress_top_bar_r(); ?>
-				<?php wp_nav_menu( array( 'theme_location' => 'top-bar-r' ) ); ?>
+				<?php foundationpress_top_bar_r(); ?>
+				<?php //wp_nav_menu( array( 'theme_location' => 'top-bar-r' ) ); ?>
 
 				<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
-					<?php // get_template_part( 'template-parts/mobile-top-bar' ); ?>
+					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
 				<?php endif; ?>
 			</div>
 			
