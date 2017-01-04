3ec3396060907c5baee6a69c10d259551f4a6eed
jamesburt85 <jamesburt85@gmail.com>
2017-01-04 19:30:29 +0000

    Single Artist adding related artists

--- wp-content/themes/askonasholt2016/single.php
+++ wp-content/themes/askonasholt2016/single.php
@@ -8,7 +8,7 @@
 
 get_header(); ?>
 
-<?php get_template_part( 'template-parts/featured-image' ); ?>
+<?php //get_template_part( 'template-parts/featured-image' ); ?>
 
 <div id="single-post" role="main">
 
@@ -16,8 +16,14 @@ get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
 	<article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
 		<header>
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-			<?php foundationpress_entry_meta(); ?>
+			
+			<h4 class="entry-title section-header"><?php the_title(); ?></h4>
+			
+			<div class="blog-meta">
+				<?php the_category(); ?>
+				<?php foundationpress_entry_meta(); ?>
+			</div>
+			
 		</header>
 		<?php do_action( 'foundationpress_post_before_entry_content' ); ?>
 		<div class="entry-content row">
@@ -43,6 +49,68 @@ get_header(); ?>
 					include(locate_template('template-parts/acf.php'));
 				}
 			?>
+			
+			<div class="artist-list">
+				<?php 
+
+					$artist = get_field('artist');
+					//print_r($artist);
+
+					if (!empty($artist)) {
+						foreach ($artist as $artist_id) { ?> 
+							
+
+								<div class="side-bar-artist"> <?php
+									# Get featured image id
+									$thumb_id = get_post_thumbnail_id($artist_id);
+									# If theere is not a featured image
+									if ( empty( $thumb_id)) {
+										$thumb_url = 'http://placehold.it/150x150';
+									# Yeay, we haven image ID
+									} else {
+										# Get the image from the image ID
+										$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail', true);
+										$thumb_url = $thumb_url_array[0];
+									}
+									//echo $thumb_url;
+
+									# Get post terms as array
+									$artist_types = get_the_terms( $artist_id, 'artist-type');
+
+									?>
+									
+									<img class="circle-thumb" src="<?php echo $thumb_url ?>">
+									
+									<div class="side-bar-artist-details">
+										<span class="side-bar-artist-name"><?php echo get_the_title( $artist_id) ?></span>&nbsp;<br/>
+									
+										<?php # If this artist has an artist-type
+										# - Will only EVER return the first result in the artist type array
+										//if ( !empty( $artist_types)): ?>
+											<span><?php //echo $artist_types[0]->name ?></span>
+										<?php //endif ?>
+
+										<span class="more-info">Visit Artist Page</span>
+										
+									</div>
+
+								</div>
+							
+				<!-- 					<?php # If the artist has an artist type
+							if ( !empty( $artist_types)): ?>
+								<ul>
+								<?php # Loop through all the artist types for this artist,
+								# - and output them all!
+								foreach ($artist_types as $type): ?>
+									<li><?php echo $type->name ?></li>
+								<?php endforeach ?>
+								</ul>
+							<?php endif ?> -->
+						
+							<?php
+						}
+					} ?>
+				</div>
 
 
 			<?php // the_content(); ?>
