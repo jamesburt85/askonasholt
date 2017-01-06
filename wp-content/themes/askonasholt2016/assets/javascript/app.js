caceeb4211d0febcf8a8915458c04eccb1133bc1
jamesburt85 <jamesburt85@gmail.com>
2017-01-06 13:32:24 +0000

    Making top -60px for in page nav scroll

--- wp-content/themes/askonasholt2016/assets/javascript/app.js
+++ wp-content/themes/askonasholt2016/assets/javascript/app.js
@@ -291,7 +291,7 @@ var map = null;
           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
           if (target.length) {
             $('html, body').animate({
-              scrollTop: target.offset().top
+              scrollTop: target.offset().top-60
             }, 1000);
             return false;
           }
@@ -460,7 +460,7 @@ var map = null;
 
 
     //*********************************************
-    //***** HIDE SHARE ICONS AFETR 200px   ********
+    //***** HIDE SHARE ICONS AFTER 200px   ********
     //*********************************************
 
     $(document).scroll(function () {
