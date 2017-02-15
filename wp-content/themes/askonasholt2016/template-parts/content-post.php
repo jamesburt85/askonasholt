0da03c60cec089a4742dec2adb051c82f41028b2
Jay <hello@jburt.co.uk>
2017-02-15 09:08:38 +0000

    Animation Tweak

--- wp-content/themes/askonasholt2016/template-parts/content-post.php
+++ wp-content/themes/askonasholt2016/template-parts/content-post.php
@@ -1,7 +1,7 @@
 
 <div class="entry-content small-12 medium-6 large-3 columns animated waypoint is-hidden-onload" id="waypoint">
 
-	<div class=" magazine-item">
+	<div class="magazine-item">
 		<?php if ( has_post_thumbnail() ) { ?>
 
 			<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
@@ -29,7 +29,7 @@
 
 		else { ?>
 			<a href="<?php the_permalink(); ?>">
-			<div class="magazine-item-thumb online-item animated waypoint is-hidden-onload" id="waypoint" style="background-image: url('<?php bloginfo('template_directory'); ?>/assets/images/default.jpg');">
+			<div class="magazine-item-thumb online-item" style="background-image: url('<?php bloginfo('template_directory'); ?>/assets/images/default.jpg');">
 				<div class="bottom-left">
 					<div class="media-type-indicator">
 						<?php if( get_field('media_choice') == 'Video' ): ?>
