9d6e286b4611652cef60ba86b21ebad65e960abe
Jay <hello@jburt.co.uk>
2017-03-09 17:39:21 +0000

    Artist Image curve

--- wp-content/themes/askonasholt2016/template-parts/single-artist-hero.php
+++ wp-content/themes/askonasholt2016/template-parts/single-artist-hero.php
@@ -25,7 +25,7 @@
 			
 			<!-- <img src="<?php //echo get_template_directory_uri(); ?>/assets/images/pattern.svg"> -->
 		
-			<div class="image-curve" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/pattern.svg')"></div>	
+			<div class="image-curve" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/pattern-3.svg')"></div>	
 
 		</div>
 
