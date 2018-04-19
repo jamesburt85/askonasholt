126dabbc2f79e33a135f95b9e0b2d138949a153e
Theo Pape <theopape@Theos-MacBook-Air.local>
2018-04-19 12:46:24 +0100

    Adding loading indicator/reshowing load more button (fallback for if AJAX scroll fails).

--- wp-content/themes/askonasholt2016/library/ajaxloadmore.php
+++ wp-content/themes/askonasholt2016/library/ajaxloadmore.php
@@ -132,9 +132,14 @@ if ( !function_exists('wiaw_universal_ajax')) {
             <div id="ajax-before-me"></div>
 
             <div class="ajax-spinner">
-                <div class="bounce1"></div>
+                <div class="ball-scale-ripple-multiple">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>                
+                <!-- <div class="bounce1"></div>
                 <div class="bounce2"></div>
-                <div class="bounce3"></div>
+                <div class="bounce3"></div> -->
             </div>
 
             <div class="archive__load-more">
